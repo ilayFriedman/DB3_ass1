8fce4a971aae9d5e3ef20f25dd106eb351169444
ilayfri <ilayfri@post.bgu.ac.il>
2019-11-20 12:49:26 +0200

    c vdvfedvf

--- src/main.java
+++ src/main.java
@@ -2,7 +2,8 @@ public class main {
 
 
     public static void main(String[]args) {
-        Assigment shit = new Assigment("jdbc:oracle:thin:@ora1.ise.bgu.ac.il:1521/ORACLE", "ilayf", "ilayfRi140");
+        Assigment shit = new Assigment("jdbc:oracle:thin:@ora1.ise.bgu.ac.il:1521/ORACLE", "ilayfri", "ilayfRi140");
+        shit.fileToDataBase("C:/Users/ilayfri.BGU-USERS/IdeaProjects/assignmentBD_1/films.csv");
     }
 
 
